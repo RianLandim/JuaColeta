49cec87bfcd86b231abe2567b37bcce8d8f5601e
Rian Dantas Landim <rianlandim222@gmail.com>
2023-12-14 16:39:02 -0300

    some adjusts in visual identity

--- fleet-management-backoffice/src/app/(auth)/layout.tsx
+++ fleet-management-backoffice/src/app/(auth)/layout.tsx
@@ -1,10 +1,14 @@
 import { LayoutProps } from "@/utils/types/layoutProps";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: LayoutProps) {
   return (
     <main className="w-full flex flex-row">
-      <aside className="w-full bg-primary"></aside>
+      <aside className="w-full bg-primary flex flex-col items-center justify-center space-y-12">
+        <Image width={250} height={250} src="/logo/bus.png" alt="Logo" />
+        <h1 className="text-3xl font-bold text-white">Fleet Management</h1>
+      </aside>
       <div className="bg-white flex flex-col items-center justify-center p-4 w-3/4 h-screen">
         {children}
       </div>
