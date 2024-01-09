f46a80496eb04cd076fdfeb3acf5bb4516343fab
Rian Landim <rianlandim222@gmail.com>
2024-01-09 14:38:07 -0300

    added a route to bring some current user infos

--- fleet-management-api/src/infra/http/controllers/user.controller.ts
+++ fleet-management-api/src/infra/http/controllers/user.controller.ts
@@ -1,9 +1,19 @@
 import { CreateUser } from '@app/usecases/user/create';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { FindUsers } from '@app/usecases/user/find';
 import { UserViewModel } from '../view-model/user.view-model';
 import { FindUserById } from '@app/usecases/user/findById';
+import { User, UserProps } from '@utils/decorator/user.decorator';
+import { JwtAuthGuard } from '@infra/authentication/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +22,8 @@ export class UserController {
     private findUsers: FindUsers,
     private findUserById: FindUserById,
   ) {}
+
+  logger = new Logger(UserController.name);
 
   @Post()
   create(@Body() data: Required<CreateUserDto>) {
@@ -23,6 +35,16 @@ export class UserController {
     const users = await this.findUsers.execute();
 
     return users.map(UserViewModel.toHttp);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('current-user')
+  async currentUser(@User() currentUser: UserProps) {
+    this.logger.debug(`current-user has id: ${currentUser.userId}`);
+
+    const user = await this.findUserById.execute({ id: currentUser.userId });
+
+    return UserViewModel.toHttp(user);
   }
 
   @Get(':id')
