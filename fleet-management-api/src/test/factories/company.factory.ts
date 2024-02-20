85c456c3338638d9edfba7ea3d51ba8117593112
Rian Landim <rianlandim222@gmail.com>
2024-02-20 11:39:37 -0300

    test(factories): create factories to user and company

---
+++ fleet-management-api/src/test/factories/company.factory.ts
@@ -0,0 +1,34 @@
+import { Address } from '@app/entities/address';
+import { Company } from '@app/entities/company';
+import { fakerPT_BR as faker } from '@faker-js/faker';
+
+type Override = Partial<Company>;
+
+export const fakerCNPJ = () =>
+  faker.helpers.fake(
+    `${faker.string.numeric(3)}.${faker.string.numeric(
+      3,
+    )}.${faker.string.numeric(3)}/${faker.string.numeric(
+      5,
+    )}-${faker.string.numeric(2)}`,
+  );
+
+export function makeCompany(override: Override) {
+  const address = new Address({
+    city: faker.location.city(),
+    country: faker.location.country(),
+    district: faker.location.county(),
+    number: faker.number.int(),
+    state: faker.location.state(),
+    street: faker.location.street(),
+    zipCode: faker.location.zipCode('#####-###'),
+  });
+
+  return new Company({
+    socialName: faker.company.name(),
+    cnpj: fakerCNPJ(),
+    addressId: faker.string.nanoid(),
+    address: address,
+    ...override,
+  });
+}
---
+++ fleet-management-api/src/test/factories/user.factory.ts
@@ -0,0 +1,15 @@
+import { User } from '@app/entities/user';
+import { faker } from '@faker-js/faker';
+
+type Override = Partial<User>;
+
+export function makeUser(override: Override) {
+  return new User({
+    name: faker.person.fullName(),
+    cellphone: faker.phone.number(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: 'COMPANY_ADMIN',
+    ...override,
+  });
+}
