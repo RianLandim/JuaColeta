b06c0b18aae4080ff10ea2fef2def44347399710
Kawanny <kawannyviturino@gmail.com>
2024-04-22 08:52:30 -0300

    feat: redesigned settings-page screen

--- fleet-management-backoffice/src/app/painel/configuracoes/page.tsx
+++ fleet-management-backoffice/src/app/painel/configuracoes/page.tsx
@@ -4,40 +4,52 @@ import Image from "next/image";
 
 export default function Settings() {
   return (
-    <main className="bg-[#181D1A]/60 border-2 border-[#8CC63F] pb-[20%] px-[5%] rounded-xl">
+    <main className="bg-[#181D1A]/60 border-2 border-[#8CC63F] pb-[18%] px-[7%] rounded-xl">
       <div className="text-main text-xl font-normal pt-[14%] w-96">
-        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline">
-          <div className="flex gap-4 pl-[27%]">
-            <Image
-              src="/settingsIcons/conta.svg"
-              alt="Conta do usuário"
-              width={27}
-              height={27}
-            />
-            <p>Conta</p>
+        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex justify-between items-center">
+          <div>
+            <p>Empresa</p>
+            <p className="font-extralight">Empresa X</p>
           </div>
-          <hr className="my-4 border-main" />
-        </div>
-        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline">
-          <div className="flex gap-5 pl-[27%]">
-            <Image
-              src="/settingsIcons/caixaDeTexto.svg"
-              alt="Notificações"
-              width={22}
-              height={22}
-            />
-            <p>Notificações</p>
-          </div>
-          <hr className="my-4 border-main" />
-        </div>
-        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex gap-4 pl-[27%]">
           <Image
-            src="/settingsIcons/ajuda.svg"
-            alt="Ajuda"
-            width={30}
-            height={30}
+            src="/settingsIcons/arrowIcon.svg"
+            alt="Seta"
+            width={20}
+            height={20}
+            className="hover:filter hover:brightness-125"
           />
-          <p>Ajuda</p>
+        </div>
+        <hr className="my-4 border-main" />
+
+        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex justify-between items-center">
+          <div>
+            <p>E-mail</p>
+            <p className="font-extralight">empresax@gmail.com</p>
+          </div>
+          <Image
+            src="/settingsIcons/arrowIcon.svg"
+            alt="Seta"
+            width={20}
+            height={20}
+            className="hover:filter hover:brightness-125"
+          />
+        </div>
+        <hr className="my-4 border-main" />
+
+        <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex justify-between items-center">
+          <p>Senha</p>
+          <Image
+            src="/settingsIcons/arrowIcon.svg"
+            alt="Seta"
+            width={20}
+            height={20}
+            className="hover:filter hover:brightness-125"
+          />
+        </div>
+        <hr className="my-4 border-main" />
+
+        <div className="text-[#CC3030] hover:cursor-pointer hover:font-extrabold hover:underline">
+          <p>Deletar conta</p>
         </div>
       </div>
     </main>
