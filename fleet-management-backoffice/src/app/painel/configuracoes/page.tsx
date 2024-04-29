89f709e94893cbee8bb40681eba38c39c356efce
Kawanny <kawannyviturino@gmail.com>
2024-04-29 15:36:15 -0300

    feat: added changeEmail-page

--- fleet-management-backoffice/src/app/painel/configuracoes/page.tsx
+++ fleet-management-backoffice/src/app/painel/configuracoes/page.tsx
@@ -1,15 +1,20 @@
-"use client";
+"use Client";
 
 import Image from "next/image";
 
-export default function Settings() {
+interface SettingsProps {
+  company: string;
+  email: string;
+}
+
+export default function Settings({ email, company }: SettingsProps) {
   return (
     <main className="bg-[#181D1A]/60 border-2 border-[#8CC63F] pb-[18%] px-[7%] rounded-xl">
       <div className="text-main text-xl font-normal pt-[14%] w-96">
         <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex justify-between items-center">
           <div>
             <p>Empresa</p>
-            <p className="font-extralight">Empresa X</p>
+            <p className="font-extralight">{company}</p>
           </div>
           <Image
             src="/settingsIcons/arrowIcon.svg"
@@ -24,15 +29,15 @@ export default function Settings() {
         <div className="hover:cursor-pointer hover:text-[#39B54A] hover:underline flex justify-between items-center">
           <div>
             <p>E-mail</p>
-            <p className="font-extralight">empresax@gmail.com</p>
+            <p className="font-extralight">{email}</p>
           </div>
-          <Image
-            src="/settingsIcons/arrowIcon.svg"
-            alt="Seta"
-            width={20}
-            height={20}
-            className="hover:filter hover:brightness-125"
-          />
+            <Image
+              src="/settingsIcons/arrowIcon.svg"
+              alt="Seta"
+              width={20}
+              height={20}
+              className="hover:filter hover:brightness-125"
+            />
         </div>
         <hr className="my-4 border-main" />
 
