5f950efe0e45eb39cc7e215cbee7f1de30e1ce01
Wesley Kelvy <lilwkelvy@gmail.com>
2024-05-01 20:49:23 -0300

    fix: adding tailwindcss for mobile

--- fleet-management-mobile/tailwind.config.js
+++ fleet-management-mobile/tailwind.config.js
@@ -1,9 +1,10 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx"
   ],
   theme: {
     extend: {},
