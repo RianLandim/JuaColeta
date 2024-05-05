2ee12bc294217426587069b87755e439335461f0
Wesley Kelvy <lilwkelvy@gmail.com>
2024-05-05 00:00:44 -0300

    adding more organization

--- fleet-management-mobile/src/app/(tabs)/notification.tsx
+++ fleet-management-mobile/src/app/(tabs)/notification.tsx
@@ -1,5 +1,4 @@
 import { ImageBackground, View, Image } from "react-native";
-import { LayoutProps } from "../../../utils/types/layoutProps";
 import Navbar from "../components/navbar";
 import CardNotification from "../components/cardNotification";
 import { useNotificationList } from "../../../hooks/queries/useNotificationList";
@@ -7,10 +6,10 @@ import { useNotificationList } from "../../../hooks/queries/useNotificationList"
 // Use on .map render for notification
 interface NotificationProps {
   text: string;
-  isNew?: boolean
+  isNew?: boolean;
 }
 
-const NotificationQuery = useNotificationList()
+const NotificationQuery = useNotificationList();
 
 // TO DO: match render for return
 export default function Notification() {
@@ -22,20 +21,32 @@ export default function Notification() {
       <Navbar />
       <View className="flex items-center space-y-5 h-full">
         <View>
-          <CardNotification color="light" text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem." isNew />
+          <CardNotification
+            color="light"
+            text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem."
+            isNew
+          />
         </View>
 
         <View>
-          <CardNotification color="dark" text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised "
-          isNew />
+          <CardNotification
+            color="dark"
+            text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised "
+            isNew
+          />
         </View>
         <View>
-          <CardNotification color="light" text="There are many variations of passages of Lorem Ipsum available, but the majority" />
+          <CardNotification
+            color="light"
+            text="There are many variations of passages of Lorem Ipsum available, but the majority"
+          />
         </View>
         <View>
-          <CardNotification text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form" color="dark" />
+          <CardNotification
+            text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form"
+            color="dark"
+          />
         </View>
-        
       </View>
     </ImageBackground>
   );
--- fleet-management-mobile/utils/api.ts
+++ fleet-management-mobile/utils/api.ts
@@ -1,4 +1,4 @@
-import { env } from "@/env";
+import { env } from "@/.env" ;
 import { z } from "zod";
 
 type FetchApiOptions<TValidator extends z.ZodTypeAny> = (
