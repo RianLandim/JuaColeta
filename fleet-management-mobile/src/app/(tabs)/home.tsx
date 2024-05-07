b77aa57ab4a9a117d625950e62fa67fc51a74b5d
Wesley Kelvy <lilwkelvy@gmail.com>
2024-05-06 22:24:25 -0300

    feat: i need a help to use the map

--- fleet-management-mobile/src/app/(tabs)/home.tsx
+++ fleet-management-mobile/src/app/(tabs)/home.tsx
@@ -4,28 +4,43 @@ import {
   requestBackgroundPermissionsAsync,
   getCurrentPositionAsync,
   LocationObject,
+  useForegroundPermissions,
+  requestForegroundPermissionsAsync,
 } from "expo-location";
-import MapView, {Marker} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { useEffect, useState } from "react";
-
-
 
 export default function Home() {
   const [location, setLocation] = useState<LocationObject | null>(null);
 
   async function requesLocationdPermissions() {
-    const { granted } = await requestBackgroundPermissionsAsync();
+    const { status } = await requestForegroundPermissionsAsync();
 
-    if (granted) {
-      const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition);
+    console.log({ status });
+
+    if (status !== "granted") {
+      console.log("nothing");
     }
+    const currentPosition = await getCurrentPositionAsync({});
+    console.log("aqui");
+    console.log(currentPosition);
+    setLocation(currentPosition);
   }
 
   useEffect(() => {
-    requesLocationdPermissions();
+    (async () => {
+      let { status } = await requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await getCurrentPositionAsync({});
+      setLocation(location);
+    })();
   }, []);
 
+  console.log({ location });
   return (
     <ImageBackground
       className="w-full h-full"
@@ -43,8 +58,12 @@ export default function Home() {
             longitudeDelta: 0.005,
           }}
         >
-          <Marker coordinate={{latitude: location.coords.latitude,
-            longitude: location.coords.longitude}} />
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
         </MapView>
       )}
     </ImageBackground>
