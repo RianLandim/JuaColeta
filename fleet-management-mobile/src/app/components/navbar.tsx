ba73a60148abadaebc9fcaf4fb22f460e874a835
Wesley Kelvy <lilwkelvy@gmail.com>
2024-05-16 16:23:58 -0300

    fix: fixed code

--- fleet-management-mobile/src/app/components/navbar.tsx
+++ fleet-management-mobile/src/app/components/navbar.tsx
@@ -1,10 +1,11 @@
 import { View, Image } from "react-native";
+import image  from "@/assets/logos/logo2.png"
 
 export default function Navbar() {
   return (
     <View className="h-16 bg-[#1E1E1E] w-full justify-between px-3 items-center flex flex-row">
       <Image
-        source={require("../../../assets/logos/logo2.png")}
+        source={image}
         className="ml-3"
       />
       <Image source={require("../../../assets/Buttons/menuBurguer.png")} />
