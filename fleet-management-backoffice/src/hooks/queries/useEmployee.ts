3130fc62aaf52c81b8b14551cf98c999e948c304
Rian Dantas Landim <rianlandim222@gmail.com>
2024-05-17 10:05:26 -0300

    feat: now is possible to register and list employees in backoffice

--- fleet-management-backoffice/src/hooks/queries/useEmployee.ts
+++ fleet-management-backoffice/src/hooks/queries/useEmployee.ts
@@ -3,32 +3,36 @@ import { useQuery } from "@tanstack/react-query";
 import { number, string, z } from "zod";
 
 const employeeValidator = z.object({
-  name: string(),
-  phone: string(),
-  plate: string(),
-  idTruck: number(),
+  id: z.string(),
+  email: z.string(),
+  name: z.string(),
+  cellphone: z.string(),
+  license: z.string().nullish(),
+  role: z.string(),
+  createdAt: z.coerce.date(),
 });
 
 type Employee = z.infer<typeof employeeValidator>;
 
 type EmployeeQueryParams = {
-  page: string;
-  offset: string;
+  id: string;
+  email: string;
   name: string;
-  idTruck: string;
+  cellphone: string;
+  license: string | null;
+  role: string;
+  createdAt: string;
 };
 
 const FETCH_USER_WITH_VEHICLES_KEY = ["fetch-user-with-vehicles"];
 
 const fetchEmployees = async (queryParams?: EmployeeQueryParams) => {
-  const [data, _] = await fetchApi(`user/user-with-vehicles`, {
+  const [data, _] = await fetchApi("user", {
     method: "GET",
     validator: employeeValidator.array(),
-    queryParams,
   });
 
   return data;
-
 };
 
 const useEmployeesList = (queryParams?: EmployeeQueryParams) =>
